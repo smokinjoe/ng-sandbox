6a241abf8956f38214d471c2f2ff7d6d20fbc7a5
Joe Ekiert <jekiert@helixeducation.com>
2014-03-06 23:17:38 -0800

    using a filter for the same reverse capabilities.  much better to use than a scope method so it is available module-wide (PS. ignore lesson numbers, Ive already lost track..

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,12 @@ myApp.factory('Data', function () {
   return { message: "I'm data from a service" };
 });
 
+myApp.filter('reverse', function () {
+  return function (text) {
+    return text.split("").reverse().join("");
+  };
+});
+
 function FirstController($scope, Data) {
   $scope.data = Data;
 }
